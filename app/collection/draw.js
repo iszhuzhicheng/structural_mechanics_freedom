858eb4eae2b620f70244b775c973f86c8ef0a511
周汉成 <308114274@qq.com>
2016-03-02 22:41:33 +0800

    directory

--- app/collection/draw.js
+++ app/collection/draw.js
@@ -2,7 +2,7 @@ define(['app/model/draw'],function(drawM){
   return new (Backbone.Collection.extend({
     model: drawM,
 
-    url:'http://zhouhansen.github.io/structural_mechanics_freedom/app.js',
+    url:'http://zhouhansen.github.io/structural_mechanics_freedom/app.json',
 
     comparator: 'order'
   }))()
